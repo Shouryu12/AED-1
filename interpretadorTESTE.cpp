48b11a52597b9a25aa7bf0b3f8ebaf43ef0f2e3c
Felipe Henrique <solelipecosta1@gmail.com>
2017-06-04 13:14:24 -0400

    Atualizado

--- interpretadorTESTE.cpp
+++ interpretadorTESTE.cpp
@@ -8,30 +8,34 @@ string print [10];
 string chamadadeFuncao[10];
 
  
- void interpretador(){
- 	 for (int i=0;i<3;i++){
- 		int tamanho = entrada[i].size();
-		switch (tamanho){
-	  case  3 :
- 	 	 cout<< "tipo : Funcao "<<endl;
- 	 	 cout<< entrada[i]<< endl;
- 	 	 break;
-		 
-   	  case  11 : 
-  	  	  cout<<"tipo: print "<<endl;
-  	  	  cout<< entrada[i] <<endl;
-  	  	  break;
-	   case 5:
-		  cout<<"tipo: Chamada de funcao"<< endl;
-		  cout<<entrada[i];
-	  	  break;
-  	  case 0:
-  	  	cout<<"linha em branco";
-	 	 break;
- 	 	 }
- 	 }
- }
- 
+  void interpretador(){
+     int tamanho,j;
+     string numero = "";
+     for (int i=0;i<3;i++){
+        tamanho = entrada[i].size();
+        if(tamanho == 3)
+        {
+            cout<< "tipo = Funcao "<<endl;
+            cout<< entrada[i]<< endl;
+        }else if(tamanho >= 11)
+        {
+            j = 10;
+            while(j <= tamanho)
+            {
+                numero += entrada[i][j];
+                j++;
+            }
+            numero += "  ";
+        }else if(tamanho == 5)
+        {
+            cout<<"tipo = Chamada de funcao"<< endl;
+            cout<<entrada[i]<<endl;
+        }else if(tamanho == 0)
+        {
+            cout<<"linha em branco"<<endl;
+        }
+     }
+ } 
  int main(){
  	interpretador();
 	 
